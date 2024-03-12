da6ba6848643fbdf7ef3e78b0a07511d1013b0c7
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-12 15:39:21 +0100

    Done exercise 1 and 2

---
+++ src/Exercise2.java
@@ -0,0 +1,72 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class Exercise2 {
+    public static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        while (true) {
+            System.out.println("Enter a number to generate a list of random numbers");
+            try {
+                n = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                logger.error("Invalid input. Please enter a number");
+            }
+        }
+
+        List<Integer> list = genList(n);
+        logger.info("List: " + list);
+        addInvertedList(list);
+        logger.info("List: " + list);
+
+        String input = "";
+        while (!input.equals("0") && !input.equals("1")) {
+            System.out.println("Enter 0 (for even) 1 (for odd) filter the list with even or odd indexes");
+            input = sc.nextLine();
+        }
+
+        if (input.equals("0")) {
+            logger.info("Even list: " + evenOddList(true, list));
+        } else {
+            logger.info("Odd list: " + evenOddList(false, list));
+        }
+        sc.close();
+
+    }
+
+    public static List<Integer> genList(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Random().nextInt(100));
+        }
+        Collections.sort(list);
+        return list;
+    }
+
+    public static void addInvertedList(List<Integer> list) {
+        List<Integer> invertedList = new ArrayList<>(list);
+
+        invertedList.sort(Collections.reverseOrder());
+
+        list.addAll(invertedList);
+    }
+
+    public static List<Integer> evenOddList(boolean even, List<Integer> list) {
+        List<Integer> evenOddList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (even && i % 2 == 0) {
+                evenOddList.add(list.get(i));
+            }
+            if (!even && i % 2 != 0) {
+                evenOddList.add(list.get(i));
+
+            }
+        }
+        return evenOddList;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
+    //import logger
+    public static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Set<String> stringHashSet = new HashSet<>();
+        Set<String> duplicateStringHashSet = new HashSet<>();
+        int n;
+        while (true) {
+            System.out.println("Enter a number of string to insert into the Set");
+            try {
+                n = Integer.parseInt(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                logger.error("Invalid input. Please enter a number");
+            }
+        }
+        logger.info("Number of string to insert: " + n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter a string to insert into the Set");
+            String input = sc.nextLine();
+            if (stringHashSet.contains(input)) {
+                duplicateStringHashSet.add(input);
+            } else {
+                stringHashSet.add(input);
+            }
+        }
+
+        logger.info("Duplicate string: " + duplicateStringHashSet);
+        logger.info("Set: " + stringHashSet);
+        logger.info("Size of Set: " + stringHashSet.size());
+        sc.close();
     }
+
+
 }
